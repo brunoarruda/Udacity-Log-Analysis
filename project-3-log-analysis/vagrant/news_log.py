5091f34bceda133d75007adc24b06db81bacd5a4
Bruno Arruda <arrudabruno10@gmail.com>
2019-05-06 17:31:51 -0300

    added query to setup paths of articles from titles

--- project-3-log-analysis/vagrant/news_log.py
+++ project-3-log-analysis/vagrant/news_log.py
@@ -8,11 +8,24 @@ DBNAME = 'news'
 db = psycopg2.connect(database=DBNAME)
 
 c = db.cursor()
-c.execute("SELECT * FROM INFORMATION_SCHEMA.tables WHERE table_schema='public';")
-colnames = [desc[0] for desc in c.description]
+
+# query 1: Quais são os três artigos mais populares de todos os tempos?
+
+# query article titles and transform them in paths as used in log table
+query_setup_paths = """
+SELECT 
+  REGEXP_REPLACE(SUBSTRING(LOWER(title), '[[:alpha:] ]+'), ' ', '-', 'g') as PATHS
+FROM articles 
+LIMIT 100;
+"""
+c.execute(query_setup_paths)
 results = c.fetchall()
 
-print(colnames)
+print(c.description)
 print(results)
+
+# query 2: Quem são os autores de artigos mais populares de todos os tempos?
+
+# query 3: Em quais dias mais de 1% das requisições resultaram em erros?
 
 db.close()
