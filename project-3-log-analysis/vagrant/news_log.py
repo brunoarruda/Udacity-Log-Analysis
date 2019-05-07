13a49c8d5babb547afcf0a522a32a60c7f9e1c47
Bruno Arruda <arrudabruno10@gmail.com>
2019-05-07 16:38:43 -0300

    Queries finished.

--- project-3-log-analysis/vagrant/news_log.py
+++ project-3-log-analysis/vagrant/news_log.py
@@ -5,52 +5,82 @@ import psycopg2
 
 DBNAME = 'news'
 
-db = psycopg2.connect(database=DBNAME)
+def print_query(title, description, results):
+  results_str = '\n'.join([' - '.join(row) for row in results])
+  print('\n{}\n\n{}'.format(title, results_str))
 
-c = db.cursor()
+def main():
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
 
-# query 1: Quais são os três artigos mais populares de todos os tempos?
+  # registering views in database  
+  sql = open('views.sql').read()
+  for query in sql.split(';'):
+    if query.strip():
+      c.execute(query)
 
-view_article_dashed_titles = """
-CREATE VIEW article_dashed_titles AS
-SELECT
-  REGEXP_REPLACE(SUBSTRING(LOWER(title), '[[:alpha:] ]+'), ' ', '-', 'g')
-  AS dashedTitles
-FROM articles;
-"""
-c.execute(view_article_dashed_titles)
-
-view_log_dashed_titles = """
-CREATE VIEW log_dashed_titles AS
-SELECT innerquery.dashedTitles
-FROM (
+  # query 1 
+  query_title = 'Quais são os três artigos mais populares de todos os tempos?'
+  
+  query_most_popular_articles = """
   SELECT
-    SUBSTRING(path, '^/article/(.*)') AS dashedTitles
-  FROM log) as innerquery
-WHERE innerquery.dashedTitles IS NOT NULL;
-"""
-c.execute(view_log_dashed_titles)
+    "article title",
+    "access count"::text || 'views'
+  FROM title_access_count_and_author
+  LIMIT 3;"""
+  c.execute(query_most_popular_articles)
+  print_query(query_title, c.description, c.fetchall())
+  
+  # query 2
+  query_title = 'Quem são os autores mais populares de todos os tempos?'
+  
+  query_authors_of_most_popular_articles = """
+  SELECT author, views::text || 'views'
+  FROM (
+    SELECT author, SUM("access count") AS views
+    FROM title_access_count_and_author
+    GROUP BY author
+    ORDER BY views
+    LIMIT 3) AS subquery;"""
 
-view_articles_title_and_access_count = """
-CREATE VIEW article_title_and_access_count AS
-SELECT articles.dashedTitles as "article title", COUNT(*) as "access count"
-FROM article_dashed_titles as articles
-INNER JOIN log_dashed_titles as log
-ON articles.dashedTitles = log.dashedTitles
-GROUP BY "article title"
-ORDER BY "access count" DESC
-LIMIT 3;
-"""
+  c.execute(query_authors_of_most_popular_articles)
+  print_query(query_title, c.description, c.fetchall())
 
-c.execute(view_articles_title_and_access_count)
-c.execute('SELECT * FROM article_title_and_access_count')
-results = c.fetchall()
+  # query 3
+  query_title = 'Em quais dias mais de 1% das requisições resultaram em erros?'
+  
+  query_days_with_server_problems = """
+  WITH
+    status_not_found AS (
+      SELECT DATE(time) AS Data, COUNT(status) as count
+      FROM log  
+      GROUP BY DATE(time), status
+      HAVING status = '404 NOT FOUND'
+      ORDER BY Data DESC
+    ),
+    status_all AS (
+      SELECT DATE(time) AS Data, COUNT(status) as count
+      FROM log  
+      GROUP BY DATE(time), status
+      ORDER BY Data DESC
+    )
+    SELECT 
+      subquery.data::TEXT,
+      CAST(subquery.porcentagem AS VARCHAR(3)) || '% de erro' AS Porcentagem
+    FROM (
+      SELECT 
+        status_all.data, 
+        (status_not_found.count/status_all.count::DECIMAL)*100 AS Porcentagem
+      FROM status_all
+      INNER JOIN status_not_found
+      ON status_all.data = status_not_found.data
+      ORDER BY porcentagem DESC, data ASC
+    ) as subquery
+    WHERE subquery.porcentagem > 1;"""
+  c.execute(query_days_with_server_problems)
+  print_query(query_title, c.description, c.fetchall())
 
-print(results)
+  db.close()
 
-# query 2: Quem são os autores de artigos mais populares de todos os tempos?
-
-
-# query 3: Em quais dias mais de 1% das requisições resultaram em erros?
-
-db.close()
+if __name__ == '__main__':
+  main()
