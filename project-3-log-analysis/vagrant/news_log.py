0d7d739cc486fa76eb332b28202009b0733b6431
Bruno Arruda <arrudabruno10@gmail.com>
2019-05-06 23:16:57 -0300

    Query 1 finished

--- project-3-log-analysis/vagrant/news_log.py
+++ project-3-log-analysis/vagrant/news_log.py
@@ -11,20 +11,45 @@ c = db.cursor()
 
 # query 1: Quais são os três artigos mais populares de todos os tempos?
 
-# query article titles and transform them in paths as used in log table
-query_setup_paths = """
-SELECT 
-  REGEXP_REPLACE(SUBSTRING(LOWER(title), '[[:alpha:] ]+'), ' ', '-', 'g') as PATHS
-FROM articles 
-LIMIT 100;
+view_article_dashed_titles = """
+CREATE VIEW article_dashed_titles AS
+SELECT
+  REGEXP_REPLACE(SUBSTRING(LOWER(title), '[[:alpha:] ]+'), ' ', '-', 'g')
+  AS dashedTitles
+FROM articles;
 """
-c.execute(query_setup_paths)
+c.execute(view_article_dashed_titles)
+
+view_log_dashed_titles = """
+CREATE VIEW log_dashed_titles AS
+SELECT innerquery.dashedTitles
+FROM (
+  SELECT
+    SUBSTRING(path, '^/article/(.*)') AS dashedTitles
+  FROM log) as innerquery
+WHERE innerquery.dashedTitles IS NOT NULL;
+"""
+c.execute(view_log_dashed_titles)
+
+view_articles_title_and_access_count = """
+CREATE VIEW article_title_and_access_count AS
+SELECT articles.dashedTitles as "article title", COUNT(*) as "access count"
+FROM article_dashed_titles as articles
+INNER JOIN log_dashed_titles as log
+ON articles.dashedTitles = log.dashedTitles
+GROUP BY "article title"
+ORDER BY "access count" DESC
+LIMIT 3;
+"""
+
+c.execute(view_articles_title_and_access_count)
+c.execute('SELECT * FROM article_title_and_access_count')
 results = c.fetchall()
 
-print(c.description)
 print(results)
 
 # query 2: Quem são os autores de artigos mais populares de todos os tempos?
+
 
 # query 3: Em quais dias mais de 1% das requisições resultaram em erros?
 
